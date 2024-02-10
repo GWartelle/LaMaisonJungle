291602af4ce5525eddd5bb1e4030d0059173bb36
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-02-10 23:33:32 +0100

    Ajout composant PlantItem

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,30 +1,34 @@
-import { plantList } from "../datas/plantList";
-import "../styles/ShoppingList.css";
+import { plantList } from '../datas/plantList'
+import '../styles/ShoppingList.css'
+import PlantItem from './PlantItem'
 
 function ShoppingList() {
-  const categories = plantList.reduce(
-    (acc, plant) =>
-      acc.includes(plant.category) ? acc : acc.concat(plant.category),
-    []
-  );
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
 
-  return (
-    <div>
-      <ul>
-        {categories.map((cat) => (
-          <li key={cat}>{cat}</li>
-        ))}
-      </ul>
-      <ul className="lmj-plant-list">
-        {plantList.map((plant) => (
-          <li key={plant.id} className="lmj-plant-item">
-            {plant.name}
-            {plant.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map(({name, id, cover, water, light}) => (
+					<PlantItem 
+            name={name}
+            id={id}
+            cover={cover}
+            water={water}
+            light={light}
+          />
+				))}
+			</ul>
+		</div>
+	)
 }
 
-export default ShoppingList;
+export default ShoppingList
